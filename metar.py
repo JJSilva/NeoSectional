d8ea9381b45bf859f2422ea3ea88fa4e67c3dd15
Justin Silva <justinsilva@Justins-MacBook-Pro-2.local>
2017-11-05 22:06:17 -0800

    test

--- metar.py
+++ metar.py
@@ -3,18 +3,8 @@ import xml.etree.ElementTree as ET
 import time
 from neopixel import *
 import sys
+import neo
 
-
-# LED strip configuration:
-LED_COUNT      = 50      # Number of LED pixels.
-LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
-#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
-LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
-LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
-LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
-LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
-LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
-LED_STRIP      = ws.WS2811_STRIP_GRB   # Strip type and colour ordering
 
 
 url = "https://aviationweather.gov/adds/dataserver_current/httpparam?datasource=metars&requestType=retrieve&format=xml&mostRecentForEachStation=constraint&hoursBeforeNow=1&stationString="
@@ -92,8 +82,6 @@ color = "0,0,0"
 for metar in metars.iter('flight_category'):
 	flightCateory = metar.text
 
-
-
 	if flightCateory == "VFR":
 		color = "255,0,0"
 	elif flightCateory == "MVFR":
@@ -107,14 +95,15 @@ for metar in metars.iter('flight_category'):
 
 
 	print "Setting light " + str(i) + " to " + flightCateory + " " + color
-
-	strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
-	strip.begin()
 	rgb = color.split(',')
-	
-	strip.show()
+	neo.setcolor(i, Color(int(rgb[0]), int(rgb[1]), int(rgb[2])))
+
+	#strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
+	#strip.begin()
+	#rgb = color.split(',')
+	#strip.show()
 	i += 1
-	strip.setPixelColor(i, Color(int(rgb[0]), int(rgb[1]), int(rgb[2])))
+	#strip.setPixelColor(i, Color(int(rgb[0]), int(rgb[1]), int(rgb[2])))
 
 print "fin"
 
