dfad93ad6390f43af79ef8b2acbbf4815ae8c712
Justin Silva <justinsilva@Justins-MacBook-Pro-2.local>
2017-11-05 23:43:10 -0800

    testing

--- metar.py
+++ metar.py
@@ -97,7 +97,7 @@ for metar in metars.iter('flight_category'):
 	
 	print "Setting light " + str(i) + " to " + flightCateory + " " + color
 	rgb = color.split(',')
-	script = "sudo python neo.py " + str(i) + " " + rgb[0] + "," + rgb[1] + "," + rgb[2] # + " &"
+	script = "sudo python neo.py " + str(i) + color # + " &"
 	print script
 	os.system(script)
 	i += 1
