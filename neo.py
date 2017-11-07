44e680f470fba49e492af9992ef56834589c33c2
Justin Silva <justinsilva@Justins-MacBook-Pro-2.local>
2017-11-06 23:38:29 -0800

    single line

--- neo.py
+++ neo.py
@@ -289,7 +289,8 @@ def neo50(strip, color, wait_ms=50):
 
 def neo(index, strip, color, wait_ms=50):
 	"""light up one pixel."""
-	strip.setPixelColor(index, color)
+	print "index: " + str(index) 
+	strip.setPixelColor(int(index), color)
 	strip.show()
 	time.sleep(wait_ms/1000.0)		
 
