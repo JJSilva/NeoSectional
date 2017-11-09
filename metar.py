88b5614ffbe37c2a512a1d3900e2548cbea314ba
Justin Silva <justinsilva@Justins-MacBook-Pro-2.local>
2017-11-09 14:08:58 -0800

    fin

--- metar.py
+++ metar.py
@@ -1,7 +1,7 @@
 import urllib2
 import xml.etree.ElementTree as ET
 import time
-from neopixel import *
+#from neopixel import *
 import sys
 import os
 
@@ -15,7 +15,7 @@ LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
 LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
-LED_STRIP      = ws.WS2811_STRIP_GRB   # Strip type and colour ordering
+#LED_STRIP      = ws.WS2811_STRIP_GRB   # Strip type and colour ordering
 		
 
 with open("airports") as f:
@@ -23,50 +23,73 @@ with open("airports") as f:
 airports = [x.strip() for x in airports]
 print airports 
 
-strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
-strip.begin()
+#strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
+#strip.begin()
 
 
 
-url = "https://aviationweather.gov/adds/dataserver_current/httpparam?datasource=metars&requestType=retrieve&format=xml&mostRecentForEachStation=constraint&hoursBeforeNow=1&stationString="
 
+mydict = {
+	"":""
+}
+
+
+url = "https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&hoursBeforeNow=1&stationString="
 for airportcode in airports:
 	print airportcode
 	url = url + airportcode + ","
 
 print url
 content = urllib2.urlopen(url).read()
-#print content
 print content
-metars = ET.fromstring(content)
+
+
+root = ET.fromstring(content)
+
+
+for metar in root.iter('METAR'):
+	
+	stationId = metar.find('station_id').text
+	flightCateory = metar.find('flight_category').text
+	print stationId + " " + flightCateory
+	
+	mydict[stationId] = flightCateory
+	#color = Color(0,0,0)
+	
+
+print mydict
 
 i = 0
-
 for airportcode in airports:
-	airport = metars.find(".//" + airportcode).text
-	print airport
-	flightCateory = metars.findtext(".//" + airportcode).text
-	color = Color(0,0,0)
-	if flightCateory == "VFR":
-		color = Color(255,0,0)
-	elif flightCateory == "MVFR":
-		color = Color(0,0,255)
-	elif flightCateory == "IFR":
-		color = Color(0,255,0)
-	elif flightCateory == "LIFR":
-		color = Color(0,128,128)
-	print "Setting light " + airportcode + " " + str(i) + " " + flightCateory + " " + str(color)
-	strip.setPixelColor(i, color)
-	strip.show()
-	i = i + 1
+	print 
+	flightCateory = mydict.get(airportcode,"No")
+	print airportcode + " " + flightCateory
 
+	if  flightCateory != "No":
+		
+		if flightCateory == "VFR":
+			print "VFR"
+		#color = Color(255,0,0)
+		elif flightCateory == "MVFR":
+		#	color = Color(0,0,255)
+			print "MVFR"
+		elif flightCateory == "IFR":
+		#	color = Color(0,255,0)
+			print "IFR"
+		elif flightCateory == "LIFR":
+			#color = Color(0,128,128)
+			print "LIFR"
+	else:
+		#color = Color(255,255,255)
+		print "N/A"
 
-
-
-
-
+	print "Setting light " + str(i) + " for " + airportcode + " " + flightCateory + " " #+ str(color)
+	#strip.setPixelColor(i, color)
+	#strip.show()
+	
+	i = i+1
+print
 print "fin"
-
 
 
 
