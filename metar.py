2a4cea865e53701848eab3127f2aed248c079c60
Justin Silva <justinsilva@Justins-MacBook-Pro-2.local>
2017-11-09 08:19:57 -0800

    testing

--- metar.py
+++ metar.py
@@ -41,23 +41,22 @@ print content
 metars = ET.fromstring(content)
 
 i = 0
-for metar in metars.iter('flight_category'):
-	flightCateory = metar.text
-	color = Color(0,0,0)
-	if flightCateory == "VFR":
-		color = Color(255,0,0)
-	elif flightCateory == "MVFR":
-		color = Color(0,0,255)
-	elif flightCateory == "IFR":
-		color = Color(0,255,0)
-	elif flightCateory == "LIFR":
-		color = Color(0,128,128)
-	print "Setting light " + airportcode + " " + str(i) + " " + flightCateory + " " + str(color)
-	strip.setPixelColor(i, color)
-	
+flightCateory = tree.find(".//flight_category").text
+color = Color(0,0,0)
+if flightCateory == "VFR":
+	color = Color(255,0,0)
+elif flightCateory == "MVFR":
+	color = Color(0,0,255)
+elif flightCateory == "IFR":
+	color = Color(0,255,0)
+elif flightCateory == "LIFR":
+	color = Color(0,128,128)
+print "Setting light " + airportcode + " " + str(i) + " " + flightCateory + " " + str(color)
+strip.setPixelColor(i, color)
 
-	strip.show()
-	i = i + 1
+
+strip.show()
+i = i + 1
 
 
 
