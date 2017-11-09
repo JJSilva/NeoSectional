e0e0d28dba68c20d3bd8cce18490c3dbd1abd733
Justin Silva <justinsilva@Justins-MacBook-Pro-2.local>
2017-11-08 20:15:49 -0800

    single line

--- metar.py
+++ metar.py
@@ -6,6 +6,18 @@ import sys
 import os
 
 
+# LED strip configuration:
+LED_COUNT      = 50      # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
+LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+LED_STRIP      = ws.WS2811_STRIP_GRB   # Strip type and colour ordering
+		
+
 
 url = "https://aviationweather.gov/adds/dataserver_current/httpparam?datasource=metars&requestType=retrieve&format=xml&mostRecentForEachStation=constraint&hoursBeforeNow=1&stationString="
 
@@ -77,15 +89,16 @@ print url
 content = urllib2.urlopen(url).read()
 print content
 
-
 metars = ET.fromstring(content)
 
 
-total = 0
 color = "0,0,0"
 
-args = ""
 
+strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
+strip.begin()
+
+i = 0
 
 for metar in metars.iter('flight_category'):
 	flightCateory = metar.text
@@ -99,10 +112,12 @@ for metar in metars.iter('flight_category'):
 		color = "0,125,125"	
 	else:
 		color = "255,255,255"
-	args = args + " " + color
-	#print args
-	print "Setting light " + str(total) + " " + flightCateory + " " + color
-	total = total+1
+
+	print "Setting light " + str(i) + " " + flightCateory + " " + color 
+
+	strip.setPixelColor(i, color)
+	strip.show()
+	i = i+1
 
 
 script = "sudo python neo.py " + str(total) + " " + args
