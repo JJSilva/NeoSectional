a3e104a7580e872eafd473350d190dfcaf2ecd39
Justin Silva <justinsilva@Justins-MacBook-Pro-2.local>
2017-11-08 20:20:47 -0800

    single line

--- metar.py
+++ metar.py
@@ -122,6 +122,8 @@ for metar in metars.iter('flight_category'):
 	blue = color.split(",")[2]
 
 	print green + " " + red + " " + blue
+
+	print Color(int(green), int(red), int(blue))
 	strip.setPixelColor(i, Color((int(green), int(red), int(blue))))
 
 	strip.show()
