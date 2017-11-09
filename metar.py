2cd03d790dd69d3296d59997aa1f6f471d7d245e
Justin Silva <justinsilva@Justins-MacBook-Pro-2.local>
2017-11-08 21:52:01 -0800

    dynamic airports

--- metar.py
+++ metar.py
@@ -32,27 +32,32 @@ for airportcode in airports:
 	print airportcode
 	url = "https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&hoursBeforeNow=1&stationString=" + airportcode
 	print url
-	content = urllib2.urlopen(url).read()
-	print content
+
+	try:
+		content = urllib2.urlopen(url).read()
+		print content
 
 
-	metars = ET.fromstring(content)
+		metars = ET.fromstring(content)
 
-	for metar in metars.iter('flight_category'):
-		flightCateory = metar.text
-		color = Color(0,0,0)
-		if flightCateory == "VFR":
-			color = Color(255,0,0)
-		elif flightCateory == "MVFR":
-			color = Color(0,0,255)
-		elif flightCateory == "IFR":
-			color = Color(0,255,0)
-		elif flightCateory == "LIFR":
-			color = Color(0,128,128)
-		print "Setting light " + str(i) + " " + flightCateory + " " + str(color)
-		strip.setPixelColor(i, color)
+		for metar in metars.iter('flight_category'):
+			flightCateory = metar.text
+			color = Color(0,0,0)
+			if flightCateory == "VFR":
+				color = Color(255,0,0)
+			elif flightCateory == "MVFR":
+				color = Color(0,0,255)
+			elif flightCateory == "IFR":
+				color = Color(0,255,0)
+			elif flightCateory == "LIFR":
+				color = Color(0,128,128)
+			print "Setting light " + str(i) + " " + flightCateory + " " + str(color)
+			strip.setPixelColor(i, color)
+			strip.show()
+	 except ValueError:
+	 	color = Color(255,255,255)
+	 	strip.setPixelColor(i, color)
 		strip.show()
-
 
 
 
