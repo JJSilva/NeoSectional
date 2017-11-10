8129f57301ba0d080f075cd09241b96afd4f66ad
Justin Silva <justinsilva@Justins-MacBook-Pro-2.local>
2017-11-09 22:41:25 -0800

    yo

--- metar.py
+++ metar.py
@@ -37,7 +37,14 @@ def wheel(pos):
 
 
 
+for j in range(256*3):
+		for i in range(strip.numPixels()):
+			strip.setPixelColor(i, wheel((int(i * 256 / strip.numPixels()) + j) & 255))
+		strip.show()
+		time.sleep(2/1000.0)
 
+
+strip.begin()
 
 
 
@@ -46,21 +53,6 @@ with open("airports") as f:
     airports = f.readlines()
 airports = [x.strip() for x in airports]
 print airports 
-
-
-
-for airportcode in airports:
-	if airportcode == "NULL":
-		continue
-	for j in range(256*3):
-		for i in range(strip.numPixels()):
-			strip.setPixelColor(i, wheel((int(i * 256 / strip.numPixels()) + j) & 255))
-		strip.show()
-		time.sleep(2/1000.0)
-
-strip.begin()
-
-
 
 
 mydict = {
