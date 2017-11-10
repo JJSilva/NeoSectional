89bbd7f4768d6790d43c22c7e00419d26faa7492
Justin Silva <justinsilva@Justins-MacBook-Pro-2.local>
2017-11-09 22:56:48 -0800

    yo

--- metar.py
+++ metar.py
@@ -24,30 +24,6 @@ strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, 
 strip.begin()
 
 
-def wheel(pos):
-	"""Generate rainbow colors across 0-255 positions."""
-	if pos < 85:
-		return Color(pos * 3, 255 - pos * 3, 0)
-	elif pos < 170:
-		pos -= 85
-		return Color(255 - pos * 3, 0, pos * 3)
-	else:
-		pos -= 170
-		return Color(0, pos * 3, 255 - pos * 3)
-
-
-
-for j in range(256*1):
-		for i in range(strip.numPixels()):
-			strip.setPixelColor(i, wheel((int(i * 256 / strip.numPixels()) + j) & 255))
-		strip.show()
-		time.sleep(2/1000.0)
-
-
-strip.begin()
-
-
-
 
 with open("airports") as f:
     airports = f.readlines()
