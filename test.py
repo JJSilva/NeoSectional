067ef5bc0862a9447400f5b53f794d3ff1ce3aa5
Justin Silva <justinsilva@Justins-MacBook-Pro-2.local>
2017-11-09 22:13:08 -0800

    null airports

--- test.py
+++ test.py
@@ -87,16 +87,16 @@ if __name__ == '__main__':
 	strip.begin()
 
 	print ('Press Ctrl-C to quit.')
-	while True:
-		print ('Color wipe animations.')
-		colorWipe(strip, Color(255, 0, 0))  # Red wipe
-		colorWipe(strip, Color(0, 255, 0))  # Blue wipe
-		colorWipe(strip, Color(0, 0, 255))  # Green wipe
-		print ('Theater chase animations.')
-		theaterChase(strip, Color(127, 127, 127))  # White theater chase
-		theaterChase(strip, Color(127,   0,   0))  # Red theater chase
-		theaterChase(strip, Color(  0,   0, 127))  # Blue theater chase
-		print ('Rainbow animations.')
-		rainbow(strip)
-		rainbowCycle(strip)
-		theaterChaseRainbow(strip)
+
+	#print ('Color wipe animations.')
+	#colorWipe(strip, Color(255, 0, 0))  # Red wipe
+	#colorWipe(strip, Color(0, 255, 0))  # Blue wipe
+	#colorWipe(strip, Color(0, 0, 255))  # Green wipe
+	#print ('Theater chase animations.')
+	#theaterChase(strip, Color(127, 127, 127))  # White theater chase
+	#theaterChase(strip, Color(127,   0,   0))  # Red theater chase
+	#theaterChase(strip, Color(  0,   0, 127))  # Blue theater chase
+	print ('Rainbow animations.')
+	#rainbow(strip)
+	rainbowCycle(strip)
+	#theaterChaseRainbow(strip)
