edc82959f40ea671a5928eea7d278c3e20d240fa
Justin Silva <justinsilva@Justins-MacBook-Pro-2.local>
2017-11-10 23:13:55 -0800

    duplicate metars

--- metar.py
+++ metar.py
@@ -36,7 +36,7 @@ mydict = {
 }
 
 
-url = "https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&hoursBeforeNow=8&stationString="
+url = "https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&hoursBeforeNow=1&stationString="
 for airportcode in airports:
 	if airportcode == "NULL":
 		continue
