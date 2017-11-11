9e1b53e03581dc038b002f3d6b9c8f3bf1954241
Justin Silva <justinsilva@Justins-MacBook-Pro-2.local>
2017-11-10 23:28:57 -0800

    duplicate metars

--- metar.py
+++ metar.py
@@ -12,7 +12,7 @@ LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
 #LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
-LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+LED_BRIGHTNESS = 128     # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 LED_STRIP      = ws.WS2811_STRIP_GRB   # Strip type and colour ordering
@@ -24,13 +24,6 @@ strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, 
 
 
 strip.begin()
-
-for i in range(LED_COUNT):
-		strip.setPixelColor(i, Color(255,255,255))
-		strip.show()
-		time.sleep(20/1000)
-
-
 
 
 with open("/NeoSectional/airports") as f:
