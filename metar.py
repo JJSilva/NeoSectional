c80749b4a0bc469066155dff76a5936727207c1d
Justin Silva <justinsilva@Justins-MacBook-Pro-2.local>
2017-11-12 19:51:48 -0800

    checkwx

--- metar.py
+++ metar.py
@@ -1,7 +1,7 @@
 import urllib2
 import xml.etree.ElementTree as ET
 import time
-#from neopixel import *
+from neopixel import *
 import sys
 import os
 
@@ -16,13 +16,13 @@ LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
 LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
-#LED_STRIP      = ws.WS2811_STRIP_GRB   # Strip type and colour ordering
+LED_STRIP      = ws.WS2811_STRIP_GRB   # Strip type and colour ordering
 		
 
 
 
-#strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
-#strip.begin()
+strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
+strip.begin()
 
 
 with open("/NeoSectional/airports") as f:
