91e8996f7e05764236fef8c2b61c2ef7eeb9d296
root <jjsilva@gmail.com>
2018-06-02 20:47:05 +0000

    rainbow transitions

--- metar.py
+++ metar.py
@@ -16,7 +16,27 @@ LED_BRIGHTNESS = 128     # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 LED_STRIP      = ws.WS2811_STRIP_GRB   # Strip type and colour ordering
-		
+
+
+
+def wheel(pos):
+        """Generate rainbow colors across 0-255 positions."""
+        if pos < 85:
+                return Color(pos * 3, 255 - pos * 3, 0)
+        elif pos < 170:
+                pos -= 85
+                return Color(255 - pos * 3, 0, pos * 3)
+        else:
+                pos -= 170
+                return Color(0, pos * 3, 255 - pos * 3)
+
+def rainbowCycle(strip, wait_ms=2, iterations=1):
+        """Draw rainbow that uniformly distributes itself across all pixels."""
+        for j in range(256*iterations):
+                for i in range(strip.numPixels()):
+                        strip.setPixelColor(i, wheel((int(i * 256 / strip.numPixels()) + j) & 255))
+                strip.show()
+                #time.sleep(wait_ms/1000.0)
 
 
 
@@ -24,6 +44,8 @@ strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, 
 
 
 strip.begin()
+
+
 
 
 with open("/NeoSectional/airports") as f:
@@ -71,6 +93,10 @@ for metar in root.iter('METAR'):
 	
 
 print mydict
+
+rainbowCycle(strip)
+
+
 
 i = 0
 for airportcode in airports:
